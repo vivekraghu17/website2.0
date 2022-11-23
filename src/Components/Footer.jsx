edae6829f669bd5f4668afcf65225ac3d09da6aa
Vivek R <31771098+vivekraghu17@users.noreply.github.com>
2022-11-23 20:02:10 +0530

    "footer-changes"

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,91 +1,24 @@
 import React from "react";
-import styles from "../Stylesheets/footer.module.scss";
 import logo from "../Assets/gritly.png";
-import { useNavigate } from "react-router-dom";
-import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-const menuOptions = [
-  {
-    key: "home",
-    label: "HOME",
-
-    route: "/",
-  },
-  {
-    key: "companies",
-    label: "COMPANIES",
-
-    route: "/employer",
-  },
-  {
-    key: "bootcamps",
-    label: "BOOTCAMPS",
-
-    route: "/bootcamp",
-  },
-  {
-    key: "candidate",
-    label: "CANDIDATE",
-
-    route: "/candidate",
-  },
-  {
-    key: "contact",
-    label: "CONTACT US",
-
-    route: "/contactus",
-  },
-];
+import styles from "../Stylesheets/footer.module.scss";
+import twitter from "../Assets/twitter.png";
+import facebook from "../Assets/facebook.png";
+import instagram from "../Assets/instagram.png";
+import linkedin from "../Assets/linkedin.png";
 
 export default function Footer() {
-  const navigate = useNavigate();
   return (
     <div className={styles["main-div"]}>
-      <div className={styles["logo-section"]}>
-        <img
-          src={logo}
-          alt="gritly"
-          className={styles["logo"]}
-          onClick={() => {
-            navigate("/");
-            window.scrollTo(0, 0);
-          }}
-        />
-      </div>
-      <div className={styles["menu-items-section"]}>
-        {menuOptions.map((item, key) => {
-          return (
-            <div
-              className={styles["item"]}
-              key={key}
-              onClick={() => {
-                navigate(`${item.route}`);
-                window.scrollTo(0, 0);
-              }}
-            >
-              {item.label}
-            </div>
-          );
-        })}
-      </div>
-      <div className={styles["divider"]} />
-      <div className={styles["copyrights-section"]}>
-        <div className={styles["left-side-section"]}>
-          © {new Date().getFullYear()} Gritly. All rights reserved
+      <div className={styles["row-1"]}>
+        <img src={logo} alt="gritlylogo" className={styles["gritly-logo"]} />
+        <div className={styles["row-2"]}>
+          <span> © {new Date().getFullYear()} Gritly. All rights reserved</span>
         </div>
         <div className={styles["social-media-wrapper"]}>
-          <FacebookRoundedIcon sx={{ color: "white", cursor: "pointer" }} />
-          <TwitterIcon
-            sx={{ color: "white", marginLeft: "30px", cursor: "pointer" }}
-          />
-          <InstagramIcon
-            sx={{ color: "white", marginLeft: "30px", cursor: "pointer" }}
-          />
-          <LinkedInIcon
-            sx={{ color: "white", marginLeft: "30px", cursor: "pointer" }}
-          />
+          <img src={facebook} alt="facebook" />
+          <img src={twitter} alt="twitter" />
+          <img src={linkedin} alt="linkedin" />
+          <img src={instagram} alt="instagram" />
         </div>
       </div>
     </div>
