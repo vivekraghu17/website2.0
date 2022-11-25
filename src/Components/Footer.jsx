460deca9e0806fc2b9290405c62229d6526274c9
vvk-147 <vivek.yaligar@fxprosinc.com>
2022-11-25 06:07:05 +0530

    footer fixation excluded responsiveness

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../Assets/gritly.png";
+import logo from "../Assets/gritly.svg";
 import styles from "../Stylesheets/footer.module.scss";
 import twitter from "../Assets/twitter.png";
 import facebook from "../Assets/facebook.png";
@@ -8,18 +8,21 @@ import linkedin from "../Assets/linkedin.png";
 
 export default function Footer() {
   return (
-    <div className={styles["main-div"]}>
-      <div className={styles["row-1"]}>
+    <div className={styles["footer-container-wrapper"]}>
+      <div className={styles["logo-wrapper"]}>
         <img src={logo} alt="gritlylogo" className={styles["gritly-logo"]} />
-        <div className={styles["row-2"]}>
-          <span> © {new Date().getFullYear()} Gritly. All rights reserved</span>
-        </div>
-        <div className={styles["social-media-wrapper"]}>
-          <img src={facebook} alt="facebook" />
-          <img src={twitter} alt="twitter" />
-          <img src={linkedin} alt="linkedin" />
-          <img src={instagram} alt="instagram" />
-        </div>
+      </div>
+      <div className={styles["copyrights-wrapper"]}>
+        <span>
+          {" "}
+          Copyright © {new Date().getFullYear()} Gritly. All rights reserved
+        </span>
+      </div>
+      <div className={styles["social-media-wrapper"]}>
+        <img src={facebook} alt="facebook" />
+        <img src={twitter} alt="twitter" />
+        <img src={linkedin} alt="linkedin" />
+        <img src={instagram} alt="instagram" />
       </div>
     </div>
   );
